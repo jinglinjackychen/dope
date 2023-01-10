f1b35ecedd99153d874aa8a2356f29eed5374471
jinglinjackychen <a2396660@gmail.com>
2023-01-10 13:59:46 +0800

    create repo

---
+++ Docker/docker_run.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+ARGS=("$@")
+
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]; then
+    xauth_list=$(xauth nlist $DISPLAY)
+    xauth_list=$(sed -e 's/^..../ffff/' <<<"$xauth_list")
+    if [ ! -z "$xauth_list" ]; then
+        echo "$xauth_list" | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+DOCKER_OPTS=
+
+# Get the current version of docker-ce
+# Strip leading stuff before the version number so it can be compared
+DOCKER_VER=$(dpkg-query -f='${Version}' --show docker-ce | sed 's/[0-9]://')
+if dpkg --compare-versions 19.03 gt "$DOCKER_VER"; then
+    echo "Docker version is less than 19.03, using nvidia-docker2 runtime"
+    if ! dpkg --list | grep nvidia-docker2; then
+        echo "Please either update docker-ce to a version greater than 19.03 or install nvidia-docker2"
+        exit 1
+    fi
+    DOCKER_OPTS="$DOCKER_OPTS --runtime=nvidia"
+else
+    if [ "$1" == "gpu" -o "$1" == "GPU" ]; then
+        DOCKER_OPTS="$DOCKER_OPTS --gpus all"
+        echo "gpus all"
+    elif [ "$1" == "cpu" -o "$1" == "CPU" ]; then
+        echo "no gpus"
+    else 
+        echo "Please choose gpu or cpu!(default is cpu)"
+    fi
+fi
+
+# Prevent executing "docker run" when xauth failed.
+if [ ! -f $XAUTH ]; then
+    echo "[$XAUTH] was not properly created. Exiting..."
+    exit 1
+fi
+
+BASH_OPTION=bash
+
+docker run \
+    -it \
+    --rm \
+    -e DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -e XAUTHORITY=$XAUTH \
+    -v "$XAUTH:$XAUTH" \
+    -v "/home/$USER/DOPE_ws:/home/kl/DOPE_ws" \
+    -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+    -v "/etc/localtime:/etc/localtime:ro" \
+    -v "/dev:/dev" \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
+    -w "/home/kl/DOPE_ws" \
+    --user "root:root" \
+    --name pnp \
+    --network host \
+    --privileged \
+    --security-opt seccomp=unconfined \
+    $DOCKER_OPTS \
+    kuolunwang/thesis:dope \
+    $BASH_OPTION
---
+++ environment.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+if [ "$1" ]; then
+    echo "ROS MASRER $1"
+    export ROS_MASTER_URI=http://$1:11311
+else
+    echo "ROS MASRER 127.0.0.1"
+    export ROS_MASTER_URI=http://127.0.0.1:11311
+fi
+
+if [ "$2" ]; then
+    echo "ROS IP $2"
+    export ROS_IP=$2
+else
+    echo "ROS IP 127.0.0.1"
+    export ROS_IP=127.0.0.1
+fi
+
+source /opt/ros/noetic/setup.bash
+source ~/DOPE_ws/devel/setup.bash
